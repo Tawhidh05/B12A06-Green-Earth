67a15eec545900be03c85c4a59ed1bc7d99bbd1a
Taj <Tawhidht05@gmail.com>
2025-12-03 08:22:19 +0600

    Finilizing the UI

---
+++ script.js
@@ -0,0 +1,30 @@
+//   <!-- Tree Card -->
+//                   <div class="card w-full sm:w-auto h-auto sm:h-[380px] bg-white shadow-md rounded-xl">
+//                       <!-- Image Section -->
+//                       <figure class="h-32 sm:h-48 w-full p-2 sm:p-4 rounded-t-xl overflow-hidden">
+//                           <img
+//                               src="https://placehold.co/600x400"
+//                               alt="Mango Tree"
+//                               class="w-full h-full object-cover rounded-lg"
+//                           >
+//                       </figure>
+//                       <!-- Card Body -->
+//                       <div class="card-body p-3 sm:p-4">
+//                           <h2 class="card-title text-base sm:text-lg font-semibold">Mango Tree</h2>
+//                           <p class="text-xs sm:text-sm text-gray-500 mb-2 sm:mb-3">
+//                               A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green
+//                           </p>
+//                           <!-- Price and Category -->
+//                           <div class="flex justify-between items-center mb-2 sm:mb-3">
+//                               <span class="badge rounded-xl bg-[#DCFCE7] text-green-800 text- text-xs sm:text-sm px-2 sm:px-3 py-1">
+//                                   Fruit Tree
+//                               </span>
+//                               <span class="font-semibold text-sm sm:text-base">৳500</span>
+//                           </div>
+//                           <!-- Add to Cart Button -->
+//                               <button class=" btn w-full bg-green-800 sm:w-auto text-white rounded-full font-normal">
+//                                   Add to Cart
+//                               </button>
+//                       </div>
+//                   </div>
+
